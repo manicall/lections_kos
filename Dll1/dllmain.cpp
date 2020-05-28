e619b23b3d9a07d67964bc2bcf548c54861a0c89
max <maks04072000@mail.ru>
2020-05-28 12:28:03 +1000

    Добавьте файлы проекта.

---
+++ Dll1/dllmain.cpp
@@ -0,0 +1,31 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+
+#include <windows.h>
+#include <stdio.h>
+int WINAPI DllMain(HINSTANCE hlnstance, DWORD fdReason, PVOID pvReserved)
+{
+    return TRUE;
+}
+//экспортировать Фильтрующую функцию
+extern "C" __declspec(dllexport) 
+LRESULT CALLBACK HookProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+    if (nCode < 0)//если nCode, не продолжать обработку
+        return CallNextHookEx(NULL, nCode, wParam, lParam);
+    if (lParam & 0x80000000) //если нажата клавиша
+    {
+        char slnfo[50];
+        //преобразовать скан-код в символ
+        UINT uMap = MapVirtualKeyW((UINT)wParam, 2);
+        if (uMap)
+            //если удалось, вывести параметр wParam как символ
+            sprintf_s(slnfo, 50, "nCode = %d, wParam: %c, lParam: %d\0", nCode, uMap, lParam);
+        else
+            //иначе, вывести параметр wParam как число
+            sprintf_s(slnfo, 50, "nCode = %d, wParam: %d, lParam: %d\0", nCode, wParam, lParam);
+                //вывести собщение на экран
+        MessageBoxA(NULL, LPCSTR(slnfo), "Keyboard hook info", MB_OK);
+    }
+    return TRUE; //заблокировать дальнейшую обработку
+}
---
+++ Project1/Source.cpp
@@ -0,0 +1,83 @@
+#include <windows.h>
+#include <iostream>
+using namespace std; //использовать пространство имён std
+//для
+#define cLibName L"C:\\Users\\max\\source\\repos\\Project1\\Debug\\Dll1.dll" //имя
+int main()
+{
+	int nMenu; 	//переменная меню
+	static HINSTANCE hLib = NULL; //дескриптор библиотеки DLL
+	static HHOOK hHook = NULL; 	//дескриптор хука
+	static BOOL bHook = FALSE; // переменная установки хука
+
+	do {
+		system("cls"); //очистить экран
+		do { //вывод меню на экран
+			cout << " Menu" <<
+				endl <<
+				endl << "l. Set Hook" <<
+				endl << "2. Unset Hook" <<
+				endl <<
+				endl << "0. Exit" <<
+				endl << endl << "Vash vybor? ";
+			cin >> nMenu; //выбор пункта меню
+		} while ((nMenu < 0) || (nMenu > 2));
+		switch (nMenu)
+		{
+		case 1: //установка хука
+			if (!bHook) //если хук не установлен
+			{
+				HOOKPROC hProc = NULL; //дескриптор фильтрующей функции
+				//загрузить библиотеку DLL
+				hLib = LoadLibrary(cLibName);
+				if (!hLib) //проверка
+				{
+					MessageBoxA(GetActiveWindow(), "Библиотека не загружена!", "Ошибка", MB_OK | MB_ICONEXCLAMATION);
+					return 1;
+				}
+				//Получить адрес фильтрующей функции
+				hProc = (HOOKPROC)GetProcAddress(hLib, "h");
+				cout << GetLastError() << endl;
+				if (!hProc)
+				{
+					MessageBoxA(GetActiveWindow(), "Функция не найдена!", "Ошибка", MB_OK | MB_ICONEXCLAMATION);
+					return 2;
+				}
+				//установить глобальный хук
+				hHook = SetWindowsHookEx(WH_KEYBOARD, hProc, hLib, NULL);
+				if (!hHook) //проверка
+				{
+					MessageBoxA(GetActiveWindow(), "Хук не установлен!", "Ошибка", MB_OK | MB_ICONEXCLAMATION);
+					return 3;
+				}
+				else bHook = TRUE;
+			}
+			break;
+		case 2: //снятие хука
+			if (bHook) //если хук установлен
+			{
+				if (UnhookWindowsHookEx(hHook)) //снять хук
+				{
+					bHook = false;
+					if (hLib) //освободить библиотеку
+						if (!FreeLibrary(hLib))
+						{ //проверка
+							MessageBoxA(GetActiveWindow(),
+								"Библиотека не освобождена!", "Ошибка", MB_OK |
+								MB_ICONEXCLAMATION);
+							return 4;
+						}
+				}
+
+				else
+				{
+					MessageBoxA(GetActiveWindow(), "Хук не снят!", "Ошибка", MB_OK | MB_ICONEXCLAMATION);
+					return 5;
+				}
+			}
+				break;
+		}//switch
+	} while (nMenu);
+	
+	return 0;
+}
